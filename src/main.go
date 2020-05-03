aef2e5e04671f2b69bca53054166ed5841de6fa2
dontbetank <59025279+dontbetank@users.noreply.github.com>
2020-05-03 11:30:39 +0300

    Small bugfix

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
@@ -12,39 +11,23 @@ import (
 func Radians(degrees float64) float64 {
 	return degrees * math.Pi / 180
 }
-=======
-	"SomeTest/pixel/pixelgl"
-	/*"math"*/
-	"github.com/faiface/pixel"
-)
-
->>>>>>> origin/pixel
 func main() {
 	pixelgl.Run(run)
 }
 
 func run() {
 	ScreenW, ScreenH := pixelgl.Monitors()[0].Size()
-<<<<<<< HEAD
 	Unit := math.Sqrt(ScreenH*ScreenW) / 200
 
 	cfg := pixelgl.WindowConfig{
 		Bounds: pixel.R(0, 0, ScreenW*0.7, ScreenH*0.6),
 		Title:  "Gotanks",
 		VSync:  true}
-=======
-	/*Unit := math.Sqrt(ScreenH * ScreenW) / 200*/
-
-	cfg := pixelgl.WindowConfig{
-		Bounds: pixel.R(0, 0, ScreenW*0.7, ScreenH*0.6),
-		Title:  "Gotanks"}
->>>>>>> origin/pixel
 	win, err := pixelgl.NewWindow(cfg)
 
 	if err != nil {
 		panic(err)
 	}
-<<<<<<< HEAD
 
 	t := Tank{
 		X:            500,
@@ -61,9 +44,6 @@ func run() {
 		win.Clear(color.RGBA{A: 0xff, R: 0xc8, G: 0xc8, B: 0xc8})
 		t.update(win)
 		t.draw(win, Unit)
-=======
-	for !win.Closed() {
->>>>>>> origin/pixel
 		win.Update()
 	}
 }
@@ -73,7 +53,6 @@ type Tank struct {
 	Y                     float64
 	ReloadTime            float64
 	CurrentReloadCoundown float64
-<<<<<<< HEAD
 	ReloadSpeed           float64
 	Speed                 float64
 	AngularSpeed          float64
@@ -81,17 +60,10 @@ type Tank struct {
 	color                 color.RGBA
 	Angle                 float64
 	Radius                float64
-=======
-	Speed                 float64
-	Angle                 float64
-	Buttons               [5]pixelgl.Button
-	color                 [3]int8
->>>>>>> origin/pixel
 	id                    int
 }
 
 func (t *Tank) move(vec pixel.Vec) {
-<<<<<<< HEAD
 	t.X += vec.X
 	t.Y += vec.Y
 }
@@ -134,20 +106,6 @@ func (t *Tank) update(window *pixelgl.Window) {
 	}
 }
 
-=======
-	t.X = vec.X
-	t.Y = vec.Y
-}
-func (t *Tank) rotate(angle float64) {
-	t.Angle = angle
-}
-func (t *Tank) draw() {
-
-}
-func (t *Tank) update() {
-
-}
->>>>>>> origin/pixel
 func (t *Tank) shoot() {
 
 }
@@ -164,26 +122,16 @@ func (b *Bullet) update() {
 	b.X += b.direction.X * b.speed
 	b.Y += b.direction.Y * b.speed
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/pixel
 func (b *Bullet) draw() {
 
 }
 
 type Controllable interface {
 	move(pixel.Vec)
-<<<<<<< HEAD
 }
 type Drawable interface {
 	draw(pixel.Target, float64)
-=======
-	rotate(float64)
-}
-type Drawable interface {
-	draw()
->>>>>>> origin/pixel
 }
 type Updatable interface {
 	update()
